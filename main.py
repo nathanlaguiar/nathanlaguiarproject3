a0c7f44f541542920f5b218b9fa97222e8032c53
nathanlaguiar <nathanlaguiar02@gmail.com>
2022-10-03 19:48:36 -0400

    MY TERRIBLE COW

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ arcade.set_background_color(arcade.color.ARMY_GREEN)
 arcade.start_render()
 arcade.draw_xywh_rectangle_filled(0,400, 650, 250,
                                   arcade.color.BLACK,)
-arcade.draw_line (100, 100, 150, 100,
-                  arcade.color.BAKER_MILLER_PINK, 6)
+
 
 
 arcade.draw_circle_outline(287,492, 49,
@@ -27,14 +26,60 @@ arcade.draw_lrtb_rectangle_filled (200,379,480,443,
                                    arcade.color.BUFF)
 #THE FOLLOWING YELLOW OUTLINE IS THE SUPER AMAZING TRACTOR BEAM THAT TOOK FAR TOO LONG TO CENTER.
 # DO NOT MOVE IT!!!
-arcade.draw_triangle_outline(40, 110, 295, 440, 590, 100,
+arcade.draw_triangle_outline(40, 110, 290, 440, 590, 100,
                              arcade.color.YELLOW_ROSE,)
+
+arcade.draw_xywh_rectangle_filled(207,200,75, 75,
+                                  arcade.color.RUBY_RED,)
+arcade.draw_line (220, 202, 270, 235,
+                  arcade.color.WOOD_BROWN, 6)
+arcade.draw_line (220, 235, 270, 202,
+                  arcade.color.WOOD_BROWN, 6)
+arcade.draw_line (270, 200, 270, 238,
+                  arcade.color.WOOD_BROWN, 5)
+arcade.draw_line (220, 238, 220, 200,
+                  arcade.color.WOOD_BROWN, 5)
+arcade.draw_line (220, 235, 270, 235,
+                  arcade.color.WOOD_BROWN, 6)
 arcade.draw_arc_filled(290, 500, 67, 34,
                        arcade.color.LEMON, 60, 340)
-arcade.draw_triangle_outline(199,147, 199, 197, 159, 199,
-                            arcade.color.IRRESISTIBLE)
-arcade.draw_xywh_rectangle_filled(107,105,25, 35,
-                                  arcade.color.TROLLEY_GREY,)
+arcade.draw_triangle_filled(319, 275, 267, 315, 220,307,
+                            arcade.color.RUBY_RED, )
+arcade.draw_circle_filled(370, 275, 10,
+                           arcade.color.GHOST_WHITE)
+arcade.draw_line (376, 273, 375, 260,
+                  arcade.color.BLACK, 2)
+#a
+arcade.draw_line (365, 273, 360, 260,
+                  arcade.color.BLACK, 2)
+#B
+arcade.draw_line (375, 280, 382, 280,
+                  arcade.color.GHOST_WHITE, 2)
+arcade.draw_triangle_filled(378, 289, 381, 294, 383, 289,
+                            arcade.color.BLACK, )
+arcade.draw_circle_filled(380, 285, 5,
+                           arcade.color.GHOST_WHITE)
+arcade.draw_circle_filled(382, 286, 2,
+                           arcade.color.BLACK)
+
+arcade.draw_triangle_filled(370, 282, 358, 276, 358, 283,
+                            arcade.color.GHOST_WHITE, )
+arcade.draw_circle_filled(370, 281, 4,
+                           arcade.color.BLACK)
+arcade.draw_circle_filled(375, 279, 3,
+                           arcade.color.BLACK)
+arcade.draw_circle_filled(370, 281, 4,
+                           arcade.color.BLACK)
+arcade.draw_circle_filled(365, 280, 3,
+                           arcade.color.BLACK)
+arcade.draw_circle_filled(368, 274, 3,
+                           arcade.color.BLACK)
+arcade.draw_circle_filled(385, 284, 2,
+                           arcade.color.RUBY_RED)
+
+
+
+
 
 
 
