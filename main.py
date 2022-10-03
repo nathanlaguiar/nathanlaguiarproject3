645f3d8bd4a5518ec1087dbac09d124769b8aa92
nathanlaguiar <nathanlaguiar02@gmail.com>
2022-10-03 18:08:04 -0400

    PACMAN UFO CHALLENGE

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import arcade
 
 
-my_window = arcade.open_window(900,900, "First Window Demo UwU")
+my_window = arcade.open_window(650,600, "First Window Demo UwU")
 
 arcade.set_background_color(arcade.color.ARMY_GREEN)
 
@@ -9,29 +9,38 @@ arcade.set_background_color(arcade.color.ARMY_GREEN)
 
 # arcade.color.
 arcade.start_render()
+arcade.draw_xywh_rectangle_filled(0,400, 650, 250,
+                                  arcade.color.BLACK,)
 arcade.draw_line (100, 100, 150, 100,
-                  arcade.color.LEMON, 1)
-arcade.draw_lrtb_rectangle_outline (40,309,68,30,
-                                   arcade.color.BUFF)
-arcade.draw_circle_outline(80,230, 45,
-                           arcade.color.KHAKI, 5)
-arcade.draw_triangle_filled(40, 610, 140, 688, 190, 600,
-                             arcade.color.YELLOW_ROSE,)
-arcade.draw_arc_filled(420, 500, 567, 134,
-                       arcade.color.BAKER_MILLER_PINK, 60, 340)
-arcade.draw_triangle_filled(799,747, 799, 797, 759, 899,
-                            arcade.color.IRRESISTIBLE)
-arcade.draw_xywh_rectangle_filled(707,105,25, 135,
-                                  arcade.color.RED,)
-arcade.draw_xywh_rectangle_filled(657,175, 125, 20,
-                                  arcade.color.RED,)
-arcade.draw_circle_filled(790, 300, 45,
-                          arcade.color.KHAKI,)
-arcade.draw_circle_outline(790, 300, 55,
-                           arcade.color.GOLD, 5)
-arcade.draw_circle_outline(790, 300, 65,
-                           arcade.color.QUARTZ, 7)
+                  arcade.color.BAKER_MILLER_PINK, 6)
 
+
+arcade.draw_circle_outline(287,492, 49,
+                           arcade.color.KHAKI, 6)
+# THE ABOVE CIRCLE IS THE COVER OF PACMAN'S UFO, WHY PACMAN IS FLYING A UFO IS BEYOND ME
+arcade.draw_circle_filled(378, 462, 19,
+                          arcade.color.RED,)
+arcade.draw_circle_filled(200, 462, 19,
+                           arcade.color.BLUEBERRY, 4)
+#the above circles were wheels for a car but became edges of the ufo
+arcade.draw_lrtb_rectangle_filled (200,379,480,443,
+                                   arcade.color.BUFF)
+#THE FOLLOWING YELLOW OUTLINE IS THE SUPER AMAZING TRACTOR BEAM THAT TOOK FAR TOO LONG TO CENTER.
+# DO NOT MOVE IT!!!
+arcade.draw_triangle_outline(40, 110, 295, 440, 590, 100,
+                             arcade.color.YELLOW_ROSE,)
+arcade.draw_arc_filled(290, 500, 67, 34,
+                       arcade.color.LEMON, 60, 340)
+arcade.draw_triangle_outline(199,147, 199, 197, 159, 199,
+                            arcade.color.IRRESISTIBLE)
+arcade.draw_xywh_rectangle_filled(107,105,25, 35,
+                                  arcade.color.TROLLEY_GREY,)
+
+
+
+arcade.draw_circle_filled(590, 545, 50,
+                           arcade.color.GHOST_WHITE,)
+arcade.draw_text("Nathan L Aguiar", 450,50)
 
 
 
